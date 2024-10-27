635958747fdf1eeb056f653bb0402407f1ebf803
xcgpseud <chris.evans92@hotmail.com>
2024-10-27 14:15:33 +0000

    Part 2 complete

--- Services/Chris/DayServices/DayTwoService.cs
+++ Services/Chris/DayServices/DayTwoService.cs
@@ -28,7 +28,22 @@ public class DayTwoService : ChrisDayService, IDayService
 
     public override string SolvePartTwo()
     {
-        return "unfinished";
+        var lines = FileHelper.GetLines(User, Day);
+        var games = lines.Select(ParseLine).ToList();
+
+        var result = 0;
+        foreach (var game in games)
+        {
+            var minimumCubes = GetLowestPossibleCubes(game);
+            var power =
+                minimumCubes[CubeColours.Red] *
+                minimumCubes[CubeColours.Green] *
+                minimumCubes[CubeColours.Blue];
+
+            result += power;
+        }
+
+        return result.ToString();
     }
 
     private static Game ParseLine(string line)
@@ -67,4 +82,27 @@ public class DayTwoService : ChrisDayService, IDayService
 
     private static bool IsHandfulAllowed(Handful handful, Dictionary<string, int> allowed) =>
         handful.Groups.All(group => allowed[group.Colour] >= group.Count);
+
+    private static Dictionary<string, int> GetLowestPossibleCubes(Game game)
+    {
+        var cubes = new Dictionary<string, int>
+        {
+            { CubeColours.Red, 0 },
+            { CubeColours.Green, 0 },
+            { CubeColours.Blue, 0 },
+        };
+
+        foreach (var handful in game.Handfuls)
+        {
+            foreach (var group in handful.Groups)
+            {
+                if (group.Count > cubes[group.Colour])
+                {
+                    cubes[group.Colour] = group.Count;
+                }
+            }
+        }
+
+        return cubes;
+    }
 }
